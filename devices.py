94403ee97cd30716566c568236f875134713630b
Michael M. Vitousek <mvitouse@indiana.edu>
2018-02-15 10:26:26 -0500

    work

---
+++ devices.py
@@ -0,0 +1,41 @@
+import time
+from retic.typing import *
+
+@fields({'record': List[Tuple[time.struct_time, Any]]})
+class Log:
+    def __init__(self:'TimedLog'):
+        self.record = []
+
+    def log(self:'TimedLog', log_time: time.struct_time, log_data)->None:
+        self.record.append((log_time, log_data))
+
+    def last_reading(self:'TimedLog')->Tuple[time.struct_time, Any]:
+        return self.record[-1]
+
+    def __len__(self:'TimedLog')->int:
+        return len(self.record)
+
+@fields({'mac': str, 'updates': Log, 'temperatures': Log})
+class Device:
+    def __init__(self:'Device', mac:str):
+        self.mac = mac
+        self.updates = Log()
+        self.temperatures = Log()
+
+    def record_temp(self:'Device', temp:float)->None:
+        cur_time = time.gmtime()
+        self.temperatures.log(cur_time, temp)
+
+    def update(self:'Device', msg:str)->None:
+        cur_time = time.gmtime()
+        self.updates.log(cur_time, msg)
+
+    def last_temp(self:'Device')->Tuple[time.struct_time, float]:
+        return self.temperatures.last_reading()
+
+    def temp_recorded(self:'Device')->bool:
+        return len(self.temperatures) > 0
+
+        
+            
+        
--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-import temp_server
+import devices
 import time, calendar
 from flask import *
 app = Flask('iottrickster')
 
 @app.route("/")
 def index():    
-    return render_template('index.html', devices=get_devices(), c_to_f=c_to_f)
+    return render_template('index.html', devices=get_devices(), c_to_f=c_to_f, gmt_to_local=gmt_to_local)
+
+@app.route('/details/<mac>')
+def details(mac):
+    return render_template('details.html', mac=mac)
 
 @app.route('/signal/temp', methods=['POST'])
 def signal_temp():
     mac = request.form['mac']
     temp = request.form['temp']
-    devices = get_devices()
-    if mac not in devices:
-        devices[mac] = temp_server.Device(mac)
-    devices[mac].record_temp(float(temp))
+    devs = get_devices()
+    if mac not in devs:
+        devs[mac] = devices.Device(mac)
+    devs[mac].record_temp(float(temp))
     return redirect(url_for('index'))
 
 def c_to_f(c:float)->float:
     return c * (9 / 5) + 32
 
 def gmt_to_local(t:time.struct_time)->time.struct_time:
-    return time.localtime(calendar.timegm(t))
-
+    tm = time.localtime(calendar.timegm(t))
+    apm = 'am'
+    if tm.tm_hour == 0:
+        tm.tm_hour = 12
+    elif tm.tm_hour >= 12:
+        apm = 'pm'
+        if tm.tm_hour > 12:
+            tm.tm_hour -= 12
+    return tm, apm
+            
 
 def get_devices():
     return app.config['DEVICES']

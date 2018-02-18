37b6f06b1d23aa50b9ef43e280b05e88f3c9010e
mvitousek <mvitouse@indiana.edu>
2018-02-18 01:21:39 -0500

    Full history logging

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ except ImportError:
     # If you don't have Reticulated Python installed
     from retic_dummies import *
 
+# Type aliases:
+DB = sql.Connection
+Row = sql.Row
+
 
 HOST, PORT = '0.0.0.0', 8713
 
@@ -37,22 +41,39 @@ def index():
         cur = db.execute('select max(id), unixtime, temperature from temp_records where mac="{}"'.format(mac))
         _, unixtime, temp = cur.fetchone()
         time, date = format_gmt_for_local(unixtime)
-        data.append((mac, alias, time, date, c_to_f(temp)))
+        data.append((mac, alias, time, date, temp))
         
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, tempformat=c_to_f)
+
+def get_logs(db:DB, mac:str, count:int, offset:int=0)->List[sql.Row]:
+    cur = db.execute('select unixtime, temperature from temp_records where mac="{}" order by id desc limit {} offset {}'.format(mac, count, offset))
+    return cur.fetchall()
+
+def get_alias(db:DB, mac:str)->str:
+    cur = db.execute('select devalias from aliases where mac="{}"'.format(mac))
+    alias, = cur.fetchone()
+    return alias
 
 @app.route('/details/<mac>')
 def details(mac:str):
-    hist_length = 12
     db = get_db()
 
-    cur = db.execute('select devalias from aliases where mac="{}"'.format(mac))
-    alias, = cur.fetchone()
-    cur = db.execute('select unixtime, temperature from temp_records where mac="{}" order by id desc limit {}'.format(mac, hist_length))
-    data = cur.fetchall()
-    assert len(data) <= hist_length
+    alias = get_alias(db, mac)
+    data = get_logs(db, mac, 12)
+    assert len(data) <= 12
 
     return render_template('details.html', mac=mac, alias=alias, data=data, tdformat=format_gmt_for_local, tempformat=c_to_f)
+
+@app.route('/details/<mac>/<count>/<offset>')
+def history(mac:str, count:int, offset:int):
+    db = get_db()
+
+    alias = get_alias(db, mac)
+    data = get_logs(db, mac, count, offset)
+    cur = db.execute('select count(*) from temp_records where mac="{}"'.format(mac, count, offset))
+    total, = cur.fetchone()
+    
+    return render_template('history.html', mac=mac, alias=alias, offset=offset, data=data, total=total, tdformat=format_gmt_for_local, tempformat=c_to_f)
 
 @app.route('/details/<mac>/set_alias', methods=['POST'])
 def set_alias(mac:str):
@@ -91,14 +112,14 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-def c_to_f(c:float)->float:
-    return c * (9 / 5) + 32
+def c_to_f(c:float)->str:
+    return '{:.1f}°F'.format(c * (9 / 5) + 32)
 
-def gmt_to_local(t:int)->time.struct_time:
-    return time.localtime(t)
+def unix_to_local(epoch:int)->time.struct_time:
+    return time.localtime(epoch)
 
-def format_gmt_for_local(t:time.struct_time)->Tuple[str,str]:
-    t = gmt_to_local(t)
+def format_gmt_for_local(epoch:int)->Tuple[str,str]:
+    t = unix_to_local(epoch)
     ampm = 'am'
     hour = t.tm_hour
     if hour == 0:
@@ -110,7 +131,7 @@ def format_gmt_for_local(t:time.struct_time)->Tuple[str,str]:
     return '{}:{:02d}:{:02d}{}'.format(hour, t.tm_min, t.tm_sec, ampm),\
            '{}/{}/{}'.format(t.tm_year, t.tm_mon, t.tm_mday)
 
-def get_db():
+def get_db()->DB:
     """Opens a new database connection if there is none yet for the
     current application context.
     """
@@ -118,7 +139,7 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-def connect_db():
+def connect_db()->DB:
     rv = sql.connect(app.config['DATABASE'])
     rv.row_factory = sql.Row
     return rv

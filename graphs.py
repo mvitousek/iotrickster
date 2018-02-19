8476633318eb145d3ac2154f19484dcab1fd64a5
mvitousek <mvitouse@indiana.edu>
2018-02-18 22:48:12 -0500

    forgot commit :/

---
+++ graphs.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+
+import json
+import plotly
+
+def graph_temp(db, mac):
+    cur = db.execute('select unixtime, temperature from temp_records where mac="{}" order by id'.format(mac))
+    data = cur.fetchall()
+
+    unixtimes, temps = zip(*data)
+    times = [datetime.fromtimestamp(t) for t in unixtimes]
+
+    graph = {
+        'data': [ {
+            'x': times,
+            'y': [c * (9 / 5) + 32 for c in temps],
+            'type': 'scatter',
+            'mode': 'lines'
+        } ],
+        'layout': {
+            'title': 'Temperature',
+            'yaxis': {
+                'autorange': True,
+                'ticksuffix': '°F'
+            },
+            'xaxis': {
+                'autorange': True,
+                'range': [times[-1] - timedelta(days=1), times[-1]],
+                'rangeselector': {'buttons': [
+                    {
+                        'count': 1,
+                        'label': '1d',
+                        'step': 'day',
+                        'stepmode': 'backward'
+                    },
+                    {
+                        'count': 3,
+                        'label': '3d',
+                        'step': 'day',
+                        'stepmode': 'backward'
+                    },
+                    {
+                        'count': 7,
+                        'label': '1w',
+                        'step': 'day',
+                        'stepmode': 'backward'
+                    },
+                    {
+                        'count': 1,
+                        'label': '1m',
+                        'step': 'month',
+                        'stepmode': 'backward'
+                    },
+                    {
+                        'count': 1,
+                        'label': '1y',
+                        'step': 'year',
+                        'stepmode': 'backward'
+                    },
+                    {'step': 'all'}
+                ]},
+                'type': 'date'
+            }
+        }
+    }
+    
+    graph_json = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+    return graph_json
